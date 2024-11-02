650858fa7f5aab55332a5bef6ac733322eca732f
MacAjwani <mayankajwani@gmail.com>
2024-11-02 14:29:08 -0500

    reverted index.tsx

--- uthealth/app/index.tsx
+++ uthealth/app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <WeightScreen/>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
