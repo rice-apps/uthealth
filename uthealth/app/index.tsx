f24104af473958142ba248a5f2e46ae90b1b6ae2
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2024-11-09 14:49:20 -0600

    added nativewind

--- uthealth/app/index.tsx
+++ uthealth/app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
+import "../global.css"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="items-center">
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
