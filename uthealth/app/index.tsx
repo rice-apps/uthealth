844625a3d7cdad2012c1d119447750099a8bb8fb
MacAjwani <mayankajwani@gmail.com>
2024-11-09 13:22:26 -0600

    fixed weight input screen

--- uthealth/app/index.tsx
+++ uthealth/app/index.tsx
@@ -3,14 +3,6 @@ import WeightScreen from "./weight-input";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   <WeightScreen/>
   );
 }
