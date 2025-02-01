7c5633eda2f1966f86556f085d2b3c1d6f1fa543
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-02-01 14:22:18 -0600

    basic user context

--- uthealth/app/index.tsx
+++ uthealth/app/index.tsx
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
+import { UserProvider } from "@/user/UserContext";
+import 'react-native-gesture-handler'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import "../global.css"
 
 export default function Index() {
   return (
-    <View className="items-center">
-      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <UserProvider>
+        <View className="items-center">
+          <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+        </View>
+      </UserProvider>
+    </GestureHandlerRootView>
   );
 }
