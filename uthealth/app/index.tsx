7f252a7a5d32df3f3288fcbcc5688ee284e26653
MacAjwani <mayankajwani@gmail.com>
2025-02-01 15:22:08 -0600

    updated component

--- uthealth/app/index.tsx
+++ uthealth/app/index.tsx
@@ -1,20 +1,18 @@
-import { Text, View } from "react-native";
-import Dateofbirth from "./dateofbirth";
-import ScrollButton from "./scrollbutton";
-import { StyleSheet } from "react-native";
+import { Text, View } from 'react-native'
+import ScrollButton from './scrollbutton'
+import { StyleSheet } from 'react-native'
+import { DobInput } from './dobInput'
 
 export default function Index() {
-  return (
-      <Dateofbirth />
-  );
+    return <DobInput />
 }
 
 const styles = StyleSheet.create({
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '90%',
-  }
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+    },
 })
